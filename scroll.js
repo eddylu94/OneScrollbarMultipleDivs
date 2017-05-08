d2944d9921980911c3798f03cfd752a83cfdc89b
Eddy Lu <eddy.lu@mail.mcgill.ca>
2017-05-07 21:39:39 -0700

    Create scrolling script along with example page
    Implement scrolling functionality for scenario where the scrollbar of a container div controls the scrolling of all of the container div's child divs

---
+++ scroll.js
@@ -0,0 +1,20 @@
+﻿var multipleScroll = function (divIds, frameId, containerId) {
+
+	var container = document.getElementById(containerId);
+	var container_clientHeight = container.clientHeight;
+
+	var frame = document.getElementById(frameId);
+	var frame_scrollTop = frame.scrollTop;
+	var frame_clientHeight = frame.clientHeight;
+	var frame_scrollPercentage = frame_scrollTop / (container_clientHeight - frame_clientHeight);
+
+	for (divElement of divs) {
+
+		var currentDiv = document.getElementById(divElement);
+		var currentDivScrollHeight = currentDiv.scrollHeight;
+		var currentDiv_clientHeight = currentDiv.clientHeight;
+		var currentDiv_scrollDestination = frame_scrollPercentage * (currentDivScrollHeight - currentDiv_clientHeight);
+
+		currentDiv.scrollTop = currentDiv_scrollDestination;
+	}
+}
