d1856947f4952fc303443595fba80d13407f8c66
Eddy Lu <eddy.lu@mail.mcgill.ca>
2017-05-09 00:01:14 -0700

    Add scrolling script for divs with different scroll heights
    Implement scrolling functionality for scenario where the scrollbar of a container div causes all of the container div's child divs to scroll by the same distance
    Container div's child divs stop scrolling when their scrollbars reach the ends of their scrollable heights
    Add example page for scrolling implementation

--- scroll.js
+++ scroll.js
@@ -1,4 +1,4 @@
-﻿var multipleScroll = function (divIds, frameId, containerId) {
+﻿var proportionalScroll = function (divIds, frameId, containerId) {
 
 	var container = document.getElementById(containerId);
 	var container_clientHeight = container.clientHeight;
@@ -16,5 +16,23 @@
 		var currentDiv_scrollDestination = frame_scrollPercentage * (currentDivScrollHeight - currentDiv_clientHeight);
 
 		currentDiv.scrollTop = currentDiv_scrollDestination;
+	}
+}
+
+var scrollThenFix = function (divIds, frameId, containerId) {
+	var container = document.getElementById(containerId);
+	var container_clientHeight = container.clientHeight;
+
+	var frame = document.getElementById(frameId);
+	var frame_scrollTop = frame.scrollTop;
+	var frame_clientHeight = frame.clientHeight;
+	var frame_scrollDistance = frame_scrollTop + frame_clientHeight;
+
+	for (divElement of divs) {
+
+		var currentDiv = document.getElementById(divElement);
+		var currentDiv_clientHeight = currentDiv.clientHeight;
+
+		currentDiv.scrollTop = frame_scrollTop;
 	}
 }
